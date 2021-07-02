c1f1e2655f5a17567dd8971687872f77c4f3d4ff
Johannes Asplund Samuelsson <johannes.aspsam@gmail.com>
2021-07-02 14:48:05 +0200

    Fixed incorrect results directory

--- source/methylmalonyl_2x10x1M.sh
+++ source/methylmalonyl_2x10x1M.sh
@@ -3,7 +3,7 @@ for i in {1..10}; do
   echo "./sampling.py --reactions data/methylmalonyl.model.tab \
   --std_drG data/methylmalonyl.model_drGs.tab \
   --ratios data/methylmalonyl.ratio_range.tab \
-  --outfile results/methylmalonyl_X/methylmalonyl.sampling.${i}.tab \
+  --outfile results/methylmalonyl_2x10x1M/methylmalonyl.sampling.${i}.tab \
   --constraints data/methylmalonyl.concentrations.tab \
   --concs results/methylmalonyl_mdf.csv --steps 1000000 --max_conc 1.1"
 done | parallel --no-notice --jobs 10
@@ -13,7 +13,7 @@ for i in {1..10}; do
   echo "./sampling.py --reactions data/methylmalonyl.model.tab \
   --std_drG data/methylmalonyl.model_drGs.tab \
   --ratios data/methylmalonyl.ratio_range.tab \
-  --outfile results/methylmalonyl_X/methylmalonyl.mdf_sampling.${i}.tab \
+  --outfile results/methylmalonyl_2x10x1M/methylmalonyl.mdf_sampling.${i}.tab \
   --constraints data/methylmalonyl.concentrations.tab \
   --concs results/methylmalonyl_mdf.csv --steps 1000000 --max_conc 1.1 \
   --mdf 1.480283"
