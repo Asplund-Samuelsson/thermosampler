eab5112fd53d28b67aec49af4f78db1345bf8a48
Johannes Asplund Samuelsson <johannes.aspsam@gmail.com>
2021-08-13 11:29:05 +0200

    Added 10 million step analysis of syntrophic model

---
+++ source/syntrophic_2x10x10M.sh
@@ -0,0 +1,21 @@
+# Sample 10 replicates of 10 million steps in parallel at feasible delta G
+for i in {1..10}; do
+  echo "./sampling.py --reactions data/syntrophic.model.tab \
+  --std_drG data/syntrophic.model_drGs.tab \
+  --ratios data/syntrophic.ratio_range.tab \
+  --outfile results/syntrophic_2x10x10M/syntrophic.sampling.${i}.tab \
+  --constraints data/syntrophic.concentrations.tab \
+  --concs results/syntrophic_mdf.csv --steps 10000000 --max_conc 1.5 \
+  --conc_sums data/syntrophic.conc_sums.tab -n 1000"
+done | parallel --no-notice --jobs 10
+
+# Sample 10 replicates of 10 million steps in parallel at optimum delta G
+for i in {1..10}; do
+  echo "./sampling.py --reactions data/syntrophic.model.tab \
+  --std_drG data/syntrophic.model_drGs.tab \
+  --ratios data/syntrophic.ratio_range.tab \
+  --outfile results/syntrophic_2x10x10M/syntrophic.mdf_sampling.${i}.tab \
+  --constraints data/syntrophic.concentrations.tab \
+  --concs results/syntrophic_mdf.csv --steps 10000000 --max_conc 1.5 \
+  --mdf 0.447935 --conc_sums data/syntrophic.conc_sums.tab -n 1000"
+done | parallel --no-notice --jobs 10
